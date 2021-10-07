70ff54f1141517529d7cb3398f08475fcb004e06
renz4ie <91771202+renz4ie@users.noreply.github.com>
2021-10-07 19:55:10 +0430

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,4 @@
-branch=main
-git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
+git clone https://github.com/renz4ie/HerokuManagerBot /root/TeamUltroid
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
