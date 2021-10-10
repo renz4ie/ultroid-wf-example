324f428c11c92a89e3a7770d2842daab4dde9e8b
renz4ie <91771202+renz4ie@users.noreply.github.com>
2021-10-10 15:44:30 +0430

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/avipatilpro/FileStoreBot
-cp ultroid/config.py /root/FileStoreBot
-cd /root/FileStoreBot
+cp ultroid/config.py FileStoreBot
+cd FileStoreBot
 pip3 install -r requirements.txt
 python3 bot.py
